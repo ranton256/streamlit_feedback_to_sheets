b324b75bcaedb92f5686b0d6323daecd9bfd7189
Richard Anton <ranton@ranton.org>
2024-10-23 10:21:40 -0700

    Create streamlit_app.py
    Initial commit of example of saving st.feedback submissions to a Google sheet.

---
+++ streamlit_app.py
@@ -0,0 +1,76 @@
+import sys
+import random
+
+import pandas as pd
+import streamlit as st
+
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+try:
+    conn = st.connection("gsheets", type=GSheetsConnection)
+except Exception as e:
+    st.write(f"Unable to connect to storage: {e}")
+    conn = None
+    print("Connection failed")
+    sys.print_exception(e)
+
+df = conn.read()
+
+st.dataframe(df)
+
+
+def append_row(df, row):
+    return pd.concat([
+        df,
+        pd.DataFrame([row], columns=row.index)]
+    ).reset_index(drop=True)
+
+
+if 'my_random_id' not in st.session_state:
+    st.session_state['my_random_id'] = random.randint(1,10000000)
+
+request_id = st.session_state['my_random_id']
+st.write(f"request_id={request_id}")
+
+with st.sidebar:
+    mask = df['request_id'].isin([request_id])
+    found = mask.any()
+    if found:
+        st.write("Thanks for your feedback!")
+        if st.button("Clear"):
+            st.session_state.pop('my_random_id')
+            st.rerun()
+
+    with st.form(key="feedback"):
+
+
+        sentiment_mapping = ["one", "two", "three", "four", "five"]
+        rating = st.feedback("stars")
+        if rating is not None:
+            st.markdown(f"You selected {sentiment_mapping[rating]} star(s).")
+            rating += 1
+
+        comment = st.text_input("Comment", "")
+        email = st.text_input("[Optional]: Enter your Email if you would like to discuss", "")
+        user = str(st.experimental_user.to_dict())
+        if st.experimental_user.email:
+            exp_user_email = st.experimental_user.email
+        st.write(f"Your exp_user_email is {exp_user_email}")
+        record = {'request_id': request_id, 'rating': rating, 'comment': comment, 'exp_user_email': exp_user_email, 'email': email}
+        new_row = pd.Series(record)
+
+        if st.form_submit_button("Submit", disabled=found):
+            if rating is None:
+                st.write("Please select a rating")
+            else:
+                print(f"Submitting record: {record}, row={new_row}")
+                df = append_row(df, new_row)
+
+                print("Submitted.")
+                df = conn.update(
+                    worksheet="Sheet1",
+                    data=df,
+                )
+                st.cache_data.clear()
+                st.rerun()
